fd680f46a57c826a15f933249ab53665234ae178
Bruno Deferrari <utizoc@gmail.com>
2020-07-17 11:10:57 -0300

    Use indexed test for repeated increment_bytes calls

--- callers/ocaml/ocaml_rust_caller.ml
+++ callers/ocaml/ocaml_rust_caller.ml
@@ -18,25 +18,20 @@ let assert_correct () =
 
 open! Core
 open Core_bench
+
 let () =
   assert_correct ();
   let n = 10 in
-  let bytes16 = Bytes.make 16 '0' in
-  let bytes32 = Bytes.make 32 '0' in
-  let bytes64 = Bytes.make 64 '0' in
-  let bytes256 = Bytes.make 256 '0' in
   let bytes256shared = Bytes.make 256 '0' in
   Command.run (Bench.make_command [
     Bench.Test.create ~name:"OCamlRS014.twice(10)" (fun () ->
       ignore (OCamlRS014.twice n));
-    Bench.Test.create ~name:"OCamlRS014.increment_bytes(bytes16, 10)" (fun () ->
-      ignore (OCamlRS014.increment_bytes bytes16 10));
-    Bench.Test.create ~name:"OCamlRS014.increment_bytes(bytes32, 10)" (fun () ->
-      ignore (OCamlRS014.increment_bytes bytes32 10));
-    Bench.Test.create ~name:"OCamlRS014.increment_bytes(bytes64, 10)" (fun () ->
-      ignore (OCamlRS014.increment_bytes bytes64 10));
-    Bench.Test.create ~name:"OCamlRS014.increment_bytes(bytes256, 10)" (fun () ->
-      ignore (OCamlRS014.increment_bytes bytes256 10));
+    Bench.Test.create_indexed
+        ~name:"OCamlRS014.increment_bytes(bytes$n, 10)"
+        ~args:[16; 32; 64; 256]
+        (fun len ->
+          let bytes = Bytes.make len '0' in
+          Staged.stage (fun () -> ignore(OCamlRS014.increment_bytes bytes 10)));
     Bench.Test.create ~name:"OCamlRS014.increment_bytes_inplace(bytes256shared, 10)" (fun () ->
       ignore (OCamlRS014.increment_bytes_inplace bytes256shared 10));
   ])
